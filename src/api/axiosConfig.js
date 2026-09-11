8d3bbc70ce313d27d4630397de22ec396a216168
SMART <126778413+smartcloud2023@users.noreply.github.com>
2026-09-11 19:27:37 +0100

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL:'http://3.16.76.30:8080',
     headers: {
         'Content-Type': 'application/json',
     },
